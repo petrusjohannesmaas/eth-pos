2c64be196675a9a14e4017d6d94b258823870df4
Petrus Maas <pjmaasdev@gmail.com>
2025-11-03 13:01:03 +0200

    Initialize Express application with user authentication views and database schema

---
+++ user-schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    UNIQUE (email)
+);
